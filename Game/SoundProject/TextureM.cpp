5c78cba088503ef616c600f278de7815160de4c0
liliancz <46685381+liliancz@users.noreply.github.com>
2020-05-11 06:09:46 -0500

    update

--- Game/SoundProject/TextureM.cpp
+++
@@ -1,8 +0,0 @@
-#include "TextureM.h"
-
-SDL_Texture* TextureM::LoadTexture(const char* filename, SDL_Renderer* ren) {
-	SDL_Surface* tmpSurface = IMG_Load(filename);
-	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, tmpSurface);
-	SDL_FreeSurface(tmpSurface);
-	return tex;
-}
---
+++ Ritmo Game 2/SoundProject/TextureM.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Game.h"
+class TextureM
+{
+public:
+	static SDL_Texture* LoadTexture(const char* filename, SDL_Renderer* ren);
+};
+
+
